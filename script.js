c223de119622b7e6123046524e4f27a490629b5c
fuad1706 <fuadshorlarxwix@gmail.com>
2023-12-24 14:22:21 -0800

    intro

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 
 
 
-var menuBar = document.querySelector(".menuBar");
-var nav = document.querySelector("nav");
+// var menuBar = document.querySelector(".menuBar");
+// var nav = document.querySelector("nav");
+// 
+// menuBar.addEventListener("click", toggleMenu);
+// 
+// function toggleMenu() {
+    // if (nav.style.display === "none" || nav.style.display === "") {
+        // nav.style.display = "flex";
+    // } else {
+        // nav.style.display = "none";
+    // }
+// }
+// 
 
-menuBar.addEventListener("click", toggleMenu);
+let menuBar = document.querySelector(".menuBar");
+let menulist = document.querySelector(".menulist");
 
-function toggleMenu() {
-    if (nav.style.display === "none" || nav.style.display === "") {
-        nav.style.display = "flex";
-    } else {
-        nav.style.display = "none";
+menuBar.onclick = function(){
+    menuBar.classList.toggle("open-menu");
+
+    if(menuBar.classList.contains("open-menu")){
+        menuBar.src = "./assets/icon-close-menu.svg";
+    }
+    else{
+        menuBar.src = "./assets/icon-menu.svg";
     }
 }
